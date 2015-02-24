e28070e62b25783d40d4725e566b1ff7f26b643f
Rafael Correa <rafinha2108@gmail.com>
2015-02-24 17:44:09 -0300

    Atualizado.

--- src/br/com/caelum/jdbc/teste/TestaInsere.java
+++ src/br/com/caelum/jdbc/teste/TestaInsere.java
@@ -8,10 +8,10 @@ import br.com.caelum.jdbc.modelo.Contato;
 public class TestaInsere {
 	public static void main(String[] args){
 		Contato contato = new Contato();
-		contato.setNome("Guilherme");
-		contato.setEmail("guilhermemoraiscorrea@gmail.com");
+		contato.setNome("Gabriel Morais Correa");
+		contato.setEmail("gabrielmoraiscorrea@gmail.com");
 		contato.setEndereco("Rua Santa Rosa n07, Santa Maria, RS");
-		contato.setDataNascimento(new GregorianCalendar(1997,0,5));
+		contato.setDataNascimento(new GregorianCalendar(1999,5,24));
 		
 		ContatoDao dao = new ContatoDao();
 		dao.adicionaContato(contato);
