860a3d88170e22b3bce35845f6fba65c8f50d6ac
Rafael Correa <rafinha2108@gmail.com>
2015-02-24 14:29:14 -0300

    Colocado senha para poder acessar banco.

--- src/br/com/caelum/jdbc/ConnectionFactory.java
+++ src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	public Connection getConnection(){
 		try{
-			return DriverManager.getConnection("jdbc:mysql://localhost/fj21","root","");
+			return DriverManager.getConnection("jdbc:mysql://localhost/fj21","root","1234");
 		}catch(SQLException ex){
 			throw new RuntimeException(ex);
 		}
